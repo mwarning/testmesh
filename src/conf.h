96c834574f24b04d1436b9c297b1e1992c5dfa8b
Moritz Warning <moritzwarning@web.de>
2022-01-02 20:07:16 +0100

    remove verbosity function declarations

--- src/conf.h
+++ src/conf.h
@@ -2,9 +2,6 @@
 #ifndef _CONF_H_
 #define _CONF_H_
 
-const char *verbosity_str(int verbosity);
-int verbosity_int(const char *verbosity);
-
 int conf_setup(int argc, char **argv);
 
 #endif // _CONF_H_
